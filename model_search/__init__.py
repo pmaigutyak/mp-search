bc1e8aa51a1723bda272bb9eed2e21fb897e7467
Paul Maigutyak <pmaigutyak@ettera.com>
2017-06-12 21:22:35 +0300

    Add search tags functionality.

--- model_search/__init__.py
+++ model_search/__init__.py
@@ -1,58 +1,20 @@
 
-import re
-import operator
-
-try:
-    from functools import reduce
-except ImportError:
-    pass
-
-from django.db.models import Q
+from model_search.lib import (
+    normalize_query, model_search, tags_search, build_query)
 
 
 __all__ = ['normalize_query', 'build_query', 'model_search']
 
-
-def normalize_query(query_string,
-                    findterms=re.compile(r'"([^"]+)"|(\S+)').findall,
-                    normspace=re.compile(r'\s{2,}').sub):
-    """
-    Splits the query string in invidual keywords, getting rid of unecessary
-    spaces and grouping quoted words together.
-    Example:
-
-    >>> normalize_query('  some random  words "with   quotes  " and   spaces')
-    ['some', 'random', 'words', 'with quotes', 'and', 'spaces']
-    """
-    return [normspace(' ', (t[0] or t[1]).strip()) for t in findterms(query_string)]
+__version__ = '2.0'
 
 
-def build_query(query_string, search_fields):
-    """
-    Returns a query, that is a combination of Q objects. That combination
-    aims to search keywords within a model by testing the given search fields.
-    """
-    terms = normalize_query(query_string)
-
-    if not terms:
-        return None
-
-    query = reduce(
-        operator.__and__,
-        (reduce(
-            operator.__or__,
-            (Q(**{"%s__icontains" % field_name: term})
-             for field_name in search_fields)
-        ) for term in terms),
-    )
-    return query
+from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
-def model_search(query, queryset, fields):
+class ModelSearchConfig(AppConfig):
+    name = 'model_search'
+    verbose_name = _("Search")
 
-    if not query:
-        return queryset
 
-    entry_query = build_query(query, fields)
-
-    return queryset.filter(entry_query)
+default_app_config = 'model_search.ModelSearchConfig'
