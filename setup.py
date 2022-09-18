142478cd88c13010442d1b37d370ce2e6769402d
Pasha Maigutyak <pmaigutyak@gmail.com>
2022-09-18 12:39:58 +0300

    Upgrade to django 4.

--- setup.py
+++ setup.py
@@ -2,24 +2,22 @@
 from setuptools import setup, find_packages
 
 
-__version__ = '2.2.1'
-
-
 with open('requirements.txt') as f:
     requires = f.read().splitlines()
 
 
+version = '3.0.0'
 url = 'https://github.com/pmaigutyak/mp-search'
 
 
 setup(
     name='django-model-search',
-    version=__version__,
+    version=version,
     description='Django search package',
     author='Paul Maigutyak',
     author_email='pmaigutyak@gmail.com',
     url=url,
-    download_url='%s/archive/%s.tar.gz' % (url, __version__),
+    download_url='%s/archive/%s.tar.gz' % (url, version),
     packages=find_packages(),
     include_package_data=True,
     license='MIT',
